c893acad0848ee98c6bc4494ff5548f393c21a5b
Jefferson Castañeda <jcastanieda@stt.com.co>
2022-09-14 15:32:58 -0500

    social and copyright

--- src/components/home/Footer/index.jsx
+++ src/components/home/Footer/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import "./index.scss";
 
 export const Footer = () => {
   return (
-    <div className="footer">
-        footer
-    </div>
+    <Box className="footer" sx={{ flexGrow: 1 }}>
+        Copyright © 2022 
+    </Box>
   );
 }
 
--- src/components/home/SocialNetworks/index.jsx
+++ src/components/home/SocialNetworks/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import logoTwitter from "../../../assets/images/logoTwitter.png";
+import { CardContent } from "@mui/material";
 
 export const SocialNetworks = () => {
   return (
-    <div className="social-networks section">
-        social networks
-    </div>
+    <Box className="nouns-sale" sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1} className="grid-container">
+        <CardContent>
+          <img className="what-is-DAU" src={logoTwitter} alt="what-is-DAU"/>
+        </CardContent>
+      </Grid>
+    </Box>
   );
 }
 
